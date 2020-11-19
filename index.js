c771515594925374cbb3ddcb4d1576685189a341
whawari <walid.hawari@imaginelabs.me>
2020-11-19 15:02:20 +0200

    testing deployment with heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // route handler with req and res arguments
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ yo: "wazaaaaap" });
 });
 
 const PORT = process.env.PORT || 5000;
