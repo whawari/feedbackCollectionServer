98281a80b505e3ac1bca8392d6341461c9a5d639
whawari <walid.hawari@imaginelabs.me>
2020-11-24 15:23:43 +0200

    [update]: encoding users

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require("../config/keys");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,7 +23,19 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleID: profile.id }).save();
+      User.findOne({ googleID: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // We already have a record with the given ID
+          // done's first argument is an error object
+          // the second argument is the user record
+          done(null, existingUser);
+        } else {
+          // We do not have a user record with this ID, make a new record (new model instance)
+          new User({ googleID: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
     }
   )
 );
