91cf35d328dd3b17a66e80cd38593fb994ef783b
whawari <walid.hawari@imaginelabs.me>
2020-11-25 09:29:15 +0200

    added auth routes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// npm packages
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+// general imports
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -7,6 +11,16 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
