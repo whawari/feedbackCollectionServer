361295a248d0e3c9941e57fefcf7deace5448486
whawari <walid.hawari@imaginelabs.me>
2020-12-01 14:34:51 +0200

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// module.export is used to export code and make it available in other files
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
