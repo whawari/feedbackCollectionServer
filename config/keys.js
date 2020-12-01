019db80133419b718eb3dee7f802a52a5dc891eb
whawari <walid.hawari@imaginelabs.me>
2020-12-01 14:49:40 +0200

    [fix]: fixed typo

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   // we are in production - return the prod set of keys
-  module.expoerts = require("./prod");
+  module.exports = require("./prod");
 } else {
   // we are in development - return the dev set of keys
-  module.expoerts = require("./dev");
+  module.exports = require("./dev");
 }
